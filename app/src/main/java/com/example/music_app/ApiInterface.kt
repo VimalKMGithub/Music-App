d0f05bfaa5649e15aa20461cb035a049d1a693d0
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2024-01-26 20:18:49 +0530

    first commit

---
+++ app/src/main/java/com/example/music_app/ApiInterface.kt
@@ -0,0 +1,13 @@
+package com.example.music_app
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface ApiInterface {
+    @Headers("your deezer Api Key",
+        "X-RapidAPI-Host: deezerdevs-deezer.p.rapidapi.com")
+    @GET("search")
+    fun getData(@Query("q")query: String):Call<MyData>
+}
